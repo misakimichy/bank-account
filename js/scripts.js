44636239ca563d24509e1c72591553b5e7cff8f7
Misaki Koonce <misaki.koonce@gmail.com>
2019-08-27 22:49:18 -0700

    Feat: Add form submit frontend logic.

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,33 @@ function BankAccount (userName, initialDeposit, balance, depositAmount, withdraw
     this.withdrawalAmount = withdrawalAmount
 }
 
+// Method to add Deposit
 BankAccount.prototype.addDeposit = function() {
     balance += this.depositAmount;
 }
 
+// Method to subtract Withdrawal
 BankAccount.prototype.subtractWithdrawal = function() {
     balance -= this.withdrawalAmount;
 }
 
 // User Interface Logic
+// Method to clear the input form
+function clearInput() {
+    $("input").val('');
+}
+$(document).ready(function(){
+    $("form#create-account").submit(function(event) {
+        event.preventDefault();
+
+        const userName = $("input#name").val();
+        const initialDeposit = parseInt($("input#initial-deposit").val());
+        let currentBalance = initialDeposit;
+
+        const newAccount = new BankAccount(userName, initialDeposit, currentBalance);
+        $(".total").show();
+        $("#transaction").show();
+        clearInput();
+        $("#output").text(`$ ${currentBalance}`);
+    });
+});
